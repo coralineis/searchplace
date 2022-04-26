533c78fa3fbdae0053e4e17cc290b194c5fe8149
coralineis <tavylrioly@gmail.com>
2022-04-26 03:23:53 +0000

    Add ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,30 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
 
- 
-  
-  
-  devise_for :admins
-  devise_for :users
+  namespace :admin do
+    resources :place_genres, only: [:create, :index, :show, :edit, :destroy]
+    resources :shop_genres, only: [:create, :show, :edit, :destroy]
+  end
+
+  scope module: :public do
+    devise_for :users, skip: [:password], controllers: {
+      registrations: 'public/registrations',
+      sessions: 'public/sessions'
+    }
+
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    resource :users, only: [:edit, :update]
+    get 'users/my_page' => 'users#show', as: 'my_page'
+    get 'users/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
+    patch 'users/withdraw' => 'users#withdraw', as: 'withdraw'
+    resources :plans
+    resources :places
+    resources :shops
+    resources :likes, only: []
+    resources :reviews, only: []
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
