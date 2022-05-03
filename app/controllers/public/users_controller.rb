5528d4b6b80b00b7c30a8bee8cee4635b940dbac
coralineis <tavylrioly@gmail.com>
2022-05-03 01:41:07 +0000

    Add userコントローラー

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,4 +2,28 @@ class Public::UsersController < ApplicationController
   def show
     @user = current_user
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to my_page_path(current_user)
+  end
+
+  def withdraw
+    @user = current_user
+    @user.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :prefecture, :email, :password)
+  end
+
 end
