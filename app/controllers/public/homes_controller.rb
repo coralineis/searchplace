d0d2ad37a52be39a630fe8ba7bd6b4753c47ca1e
coralineis <tavylrioly@gmail.com>
2022-06-09 07:39:22 +0000

    Add plan名で検索

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,11 @@
 class Public::HomesController < ApplicationController
+  before_action :search
+
+  def search
+    @q = Plan.ransack(params[:q])
+  end
+
   def top
-    @plans = Plan.all
+    @plans = @q.result(distinct: true)
   end
 end
