02b6551f82d0fcf2012832ae4321dca117b61b9a
coralineis <tavylrioly@gmail.com>
2022-06-10 03:02:32 +0000

    Fix plan一覧とtopページ,place一覧ページをまとめる

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,14 +19,12 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-$(document).on('turbolinks:load', function() {
-  $(function() {
-    $('.tab').click(function(){
-      $('.tab-active').removeClass('tab-active');
-      $(this).addClass('tab-active');
-      $('.box-show').removeClass('box-show');
-      const index = $(this).index();
-      $('.tabbox').eq(index).addClass('box-show');
-    });
-  });
+$('#tab-contents .tab[id != "tab1"]').hide();
+
+$('#tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
 });
