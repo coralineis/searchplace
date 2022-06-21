48f73977b5ab1af55b3445829b11e20cdb3f0c5e
coralineis <tavylrioly@gmail.com>
2022-06-21 09:13:39 +0000

    Add carrierwave 設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,9 +8,9 @@ CarrierWave.configure do |config|
     config.fog_directory  = 'searchplace'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
-      region: 'ap-northeast-1', 
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1',
       path_style: true
     }
 end
