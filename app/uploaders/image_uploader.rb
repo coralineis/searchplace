25b36f9116c15f14688b82c287b083d9979c8daf
coralineis <tavylrioly@gmail.com>
2022-06-21 07:37:37 +0000

    Add cacheの保存先指定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,6 +14,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def cache_dir
+    "uploads/cache/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
   def default_url
   "no_image.jpg"
   end
